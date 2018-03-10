d647a9dd069f1dd31a73a9f5c3fa14d9e84efe87
EmelyMelo <emely.melo1036@gmail.com>
2018-03-10 11:58:01 -0300

    10/03

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 //ANGULAR
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 //PRIMENG
 import {AccordionModule} from 'primeng/accordion';    
 import {MenuItem} from 'primeng/api'; 
+import {InputTextModule} from 'primeng/inputtext';
+import {PasswordModule} from 'primeng/password';
+import {ButtonModule} from 'primeng/button';
+
 //ROTAS
 import { routing } from './app.routing';
 //SERVIÇO
@@ -21,7 +27,11 @@ import { TelaInicialComponent } from './tela-inicial/tela-inicial.component';
     TelaInicialComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputTextModule,
+    FormsModule,
+    PasswordModule,
+    ButtonModule
   ],
   providers: [UsuarioService],
   bootstrap: [AppComponent]
--- src/app/tela-inicial/tela-inicial.component.ts
+++ src/app/tela-inicial/tela-inicial.component.ts
@@ -1,4 +1,6 @@
+import { UsuarioService } from './../usuario.service';
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../model/Usuario';
 
 @Component({
   selector: 'app-tela-inicial',
@@ -6,16 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tela-inicial.component.css']
 })
 export class TelaInicialComponent implements OnInit {
+  usuario: Usuario;
+  usuarios: Usuario[];
 
-  constructor() { }
+  constructor(private usuarioService: UsuarioService) {
+    this.usuarios = [];
+		this.usuario = {email:"", nome:"", senha:""};
+   }
 
   ngOnInit() {
   }
+  entrar(){
+    console.log("entrooouu");
+  }
   salvar(){
+    console.log("salvooouu");
 
   }
-  entrar(){
-    
-  }
+  
 
 }
