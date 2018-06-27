ccd459b34f3c9f1000600d3f557b6b7c518bc541
evicsouza <evavic.ifpe@gmail.com>
2018-06-27 09:52:28 -0300

    Criação do authService

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Router } from "@angular/router";
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import { Observable } from 'rxjs/Observable';
+
 
 @Injectable()
 export class AuthService {
-
-  constructor() { }
-
+  private user: Observable<firebase.User>;
+  
+  constructor(private _firebaseAuth: AngularFireAuth, private router: Router) { 
+      this.user = _firebaseAuth.authState;
+  }
 }
