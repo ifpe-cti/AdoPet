53fbf9d7bb331a8ae11d586b8fdc3584cd150b14
evicsouza <evavic.ifpe@gmail.com>
2018-12-10 15:53:30 -0200

    alguma coisa do status

--- src/app/feed/meus-pedidos-adocao/meus-pedidos-adocao.component.ts
+++ src/app/feed/meus-pedidos-adocao/meus-pedidos-adocao.component.ts
@@ -4,6 +4,9 @@ import { Component, OnInit } from '@angular/core';
 import { PedidosAdocaoService } from '../../services/pedidos-adocao.service';
 import { AuthService } from '../../services/auth.service';
 import { UsuarioService } from '../../services/usuario.service';
+import { AdocaoService } from '../../services/adocao.service';
+import { Adocao } from '../../model/Adocao';
+import { RouterLinkActive, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-meus-pedidos-adocao',
@@ -14,37 +17,59 @@ export class MeusPedidosAdocaoComponent implements OnInit {
   listaDePedidos: any[] = [];
   status: string;
   pedido: PedidosAdocao;
+  adocoes: Adocao[];
+  id: string;
 
-  constructor(private pedidosService: PedidosAdocaoService, private usuarioService:UsuarioService,
-     private authService: AuthService, private animalService:AnimalService) { 
-     }
+  constructor(private pedidosService: PedidosAdocaoService, private usuarioService: UsuarioService,
+    private authService: AuthService, private animalService: AnimalService,
+    private adocaoService: AdocaoService, private route: ActivatedRoute) {
 
-  ngOnInit() {
-    this.listar();    
   }
 
-  getUsuario(id){
-    let texto: any;
-    this.usuarioService.listarUsuario(id).subscribe(resultado=>{
+  ngOnInit() {
+    this.carregarAdocoes();
+    this.listar();
+  }
+
+  getUsuario(id) {
+    let texto: String;
+    this.usuarioService.listarUsuario(id).subscribe(resultado => {
       texto = resultado.displayName;
     });
   }
+  private carregarAdocoes() {
+    this.adocaoService.listarTodosAdocao()
+      .toPromise()
+      .then(lista => {
+        this.adocoes = lista;
+        this.carregarPedidos();
+      });
+  }
+  private carregarPedidos() {
+    this.route.params.subscribe(
+      (params: any) => {
+        this.id = params['id'];
+        this.listar();
+      });
+  }
 
-  listar(){
+  listar() {
     this.pedidosService.listarPorIdUsuario(this.authService.getUsuarioLogado()).subscribe(listaDePedidos => {
-      this.listaDePedidos = listaDePedidos;      
-      for(let i = 0; i < this.listaDePedidos.length; i++){
-          this.animalService.listarId(this.listaDePedidos[i].idAnimal).subscribe(animal=>{
-            this.listaDePedidos[i].animal = animal;
-          });
+      this.listaDePedidos = listaDePedidos;
+      for (let i = 0; i < this.listaDePedidos.length; i++) {
+        this.animalService.listarId(this.listaDePedidos[i].idAnimal).subscribe(animal => {
+          this.listaDePedidos[i].animal = animal;
+        });
       }
-
-
+      this.listaDePedidos.forEach(pedido => {
+        const adocoes = this.adocoes.filter(a => a.idPedido === pedido.id);
+        pedido['status'] = adocoes.length !== 0 ? 'Adotado' : 'Pendente';
+      });
     });
   }
-  verificaStatus(){
+  verificaStatus() {
     //só quando aceitar
-    
+
   }
 
 }
