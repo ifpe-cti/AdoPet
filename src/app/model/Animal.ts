286bc47bc0cc1ab6ed446b9c4f3e9203d41b7189
Emely Melo <emely.melo1036@gmail.com>
2018-12-11 03:03:02 -0300

    model

--- src/app/model/Animal.ts
+++ src/app/model/Animal.ts
@@ -9,25 +9,5 @@ export class Animal {
     descricao: String;
     idUsuario?: String;
 
-    constructor(){ }
-
-  /* validar(){
-        if(this.nome == "" || this.tipo == "" || this.sexo == "" || this.cor == ""|| this.idade == "" ||
-         this.porte == "" || this.descricao == ""){
-            return false;
-        }
-        return true;
-    }
-    toDocument(){
-        return {
-            nome: this.nome,
-            tipo: this.tipo,
-            sexo: this.sexo,
-            cor: this.cor,
-            idade: this.idade,
-            porte: this.porte,
-            descricao: this.descricao,
-            idUsuario: this.idUsuario
-        }
-    } */
+    constructor(){}
 }
